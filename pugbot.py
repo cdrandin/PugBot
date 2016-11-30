3ccf65f175c5cef1b38159560b2557ef3ea75a72
christopher randin <cdrandin@hotmail.com>
2016-11-29 16:24:24 -0800

    use with on json file, automatically closing it

--- pugbot.py
+++ pugbot.py
@@ -1,11 +1,14 @@
 import json
 # Commands #
 from commands.pug_orm import pug
+from commands.utils import pug_set_calls
 
 import discord
 
-config = json.loads(open('config.json').read())  # Load Configs
-DISCORD_TOKEN = config["discord_token"]
+with open('config.json', 'r') as infile:
+    config = json.loads(infile.read())  # Load Configs
+    DISCORD_TOKEN = config["discord_token"]
+
 client = discord.Client()
 
 
@@ -19,7 +22,6 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-
     if message.content.startswith('!info') or message.content.startswith('!help'):
         await client.send_message(message.channel, "I'm PugBot, the pug analyzer!\n"
                                                    "Use: !pug <name> <server> \n"
